e00e37bf62db534a9ced19e715f4da14087de96c
Julian Mendez <julian.mendez@gmail.com>
2020-11-12 21:42:51 +0100

    Add method to assume the output file

--- translator/src/main/scala/se/umu/cs/rai/scopus/translator/Main.scala
+++ translator/src/main/scala/se/umu/cs/rai/scopus/translator/Main.scala
@@ -8,7 +8,28 @@ import java.nio.file.{Files, Paths}
  */
 case class Main() {
 
-  val Help: String = "\n\nparameters: INPUT OUTPUT\n\nwhere\n\n  INPUT is the Scopus input file\n  OUTPUT is the Scala output file\n\n"
+  val ScopusExtension: String = ".scopus"
+  val ScalaExtension: String = ".scala"
+
+  val Help: String = "\n" +
+    "\nUsage:" +
+    "\n  scopus SCOPUS_SCALA_INPUT" +
+    "\nor" +
+    "\n  scopus SCOPUS_INPUT SCALA_OUTPUT" +
+    "\n" +
+    "\nwhere" +
+    "\n" +
+    "\n  SCOPUS_SCALA_INPUT is used to create the Scopus input file and Scala output file. " +
+    "\nIf the extension is " + ScopusExtension + ", the output file has extension " + ScalaExtension + "." +
+    "\nOtherwise, the extension " + ScopusExtension + " and " + ScalaExtension + " are appended to create the input and output files respectively." +
+    "\n" +
+    "\n" +
+    "\n  SCOPUS_INPUT is the Scopus input file, regardless of the extension" +
+    "\n" +
+    "\n  SCALA_OUTPUT is the Scala output file, regardless of the extension" +
+    "\n" +
+    "\n"
+
 
   val getTitleAndVersion: String = {
     val packg = this.getClass.getPackage
@@ -18,14 +39,35 @@ case class Main() {
   }
 
   def run(args: Array[String]): Unit = {
-    if (args.length == 2) {
-      val input = readFile(args(0))
-      val output = MicroTranslator().translateProgram(input)
-      val writer = new FileWriter(args(1))
-      writer.write(output)
-      writer.flush()
+    if (args.length == 1) {
+      val (inputFileName, outputFileName) = getInputOutputFileNames(args(0))
+      translate(inputFileName, outputFileName)
+
+    } else if (args.length == 2) {
+      val inputFileName = args(0)
+      val outputFileName = args(1)
+      translate(inputFileName, outputFileName)
+
+    } else {
+      println(getTitleAndVersion + Help)
+
     }
-    else println(getTitleAndVersion + Help)
+  }
+
+  def getInputOutputFileNames(inputName: String): (String, String) = {
+    if (inputName.endsWith(ScopusExtension)) {
+      (inputName, inputName.substring(0, inputName.length - ScopusExtension.length) + ScalaExtension)
+    } else {
+      (inputName + ScopusExtension, inputName + ScalaExtension)
+    }
+  }
+
+  def translate(inputFileName: String, outputFileName: String): Unit = {
+    val input = readFile(inputFileName)
+    val output = MicroTranslator().translateProgram(input)
+    val writer = new FileWriter(outputFileName)
+    writer.write(output)
+    writer.flush()
   }
 
   def readFile(fileName: String): String = {
