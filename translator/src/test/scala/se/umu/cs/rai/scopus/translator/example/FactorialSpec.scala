b48d56e5526734ea83d73bf8f425a3be7ee2fb8f
Julian Mendez <julian.mendez@gmail.com>
2021-01-30 21:13:03 +0100

    Update unit test

--- translator/src/test/scala/se/umu/cs/rai/scopus/translator/example/FactorialSpec.scala
+++ translator/src/test/scala/se/umu/cs/rai/scopus/translator/example/FactorialSpec.scala
@@ -1,16 +1,16 @@
 package se.umu.cs.rai.scopus.translator.example
 
-import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.funsuite.AnyFunSuite
 
 import scala.language.implicitConversions
 
-case class FactorialSpec() extends AnyFunSpec {
+case class FactorialSpec() extends AnyFunSuite {
 
   val factorialValues = Seq(
     (0, 1), (1, 1), (2, 2), (3, 6), (4, 24), (5, 120), (6, 720), (7, 5040), (8, 40320), (9, 362880), (10, 3628800)
   )
 
-  it("should test the factorial - concise version") {
+  test("should test the factorial - concise version") {
     val expected = factorialValues
     val obtained = factorialValues
       .map(pair => pair._1)
@@ -18,7 +18,7 @@ case class FactorialSpec() extends AnyFunSpec {
     assert(obtained === expected)
   }
 
-  it("should test the factorial - verbose version") {
+  test("should test the factorial - verbose version") {
     val expected = factorialValues
     val obtained = factorialValues
       .map(pair => pair._1)
