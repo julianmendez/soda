1245f063bcc8027bdd59adfd2a924fd6287d30b3
Julian Mendez <julian.mendez@gmail.com>
2021-02-01 15:11:11 +0100

    Add Scopus source code for example of lists

--- library/src/test/scala/se/umu/cs/rai/scopus/collection/example/ListExample.scala
+++ library/src/test/scala/se/umu/cs/rai/scopus/collection/example/ListExample.scala
@@ -1,4 +1,4 @@
-package se.umu.cs.rai.scopus.collection
+package se.umu.cs.rai.scopus.collection.example
 
 case class ListExample() {
 
@@ -78,39 +78,3 @@ case class ListExample() {
     ("A.foldRight(Seq('*'))((elem, list) => \"(\" + elem + \" +: \" + list + \")\")", A.foldRight(Seq('*'))((elem, list) => "(" + elem + " +: " + list + ")"))
 
 }
-
-object ListExample {
-
-  def main(args: Array[String]): Unit = {
-    val e = ListExample()
-    val allExamples = Seq(
-      e.aExample,
-      e.bExample,
-      e.takeExample,
-      e.takeRightExample,
-      e.takeWhileExample,
-      e.dropExample,
-      e.dropRightExample,
-      e.dropWhileExample,
-      e.splitAtExample,
-      e.zipWithIndexExample,
-      e.indicesExample,
-      e.zipExample,
-      e.reverseExample,
-      e.prependExample,
-      e.appendExample,
-      e.concatExample,
-      e.spanExample,
-      e.mapExample,
-      e.filterExample,
-      e.foldExample,
-      e.foldLeftExample,
-      e.foldRightExample
-    )
-      .map(pair => pair._1 + " = " + pair._2.toString)
-      .mkString("\n")
-    println(allExamples)
-  }
-
-}
-
---
+++ library/src/test/scala/se/umu/cs/rai/scopus/collection/example/Main.scala
@@ -0,0 +1,40 @@
+package se.umu.cs.rai.scopus.collection.example
+
+object EntryPoint {
+  def main(args: Array[String]): Unit = Main().main(args)
+}
+
+case class Main() {
+
+  def main(args: Array[String]): Unit = {
+    val e = ListExample()
+    val allExamples = Seq(
+      e.aExample,
+      e.bExample,
+      e.takeExample,
+      e.takeRightExample,
+      e.takeWhileExample,
+      e.dropExample,
+      e.dropRightExample,
+      e.dropWhileExample,
+      e.splitAtExample,
+      e.zipWithIndexExample,
+      e.indicesExample,
+      e.zipExample,
+      e.reverseExample,
+      e.prependExample,
+      e.appendExample,
+      e.concatExample,
+      e.spanExample,
+      e.mapExample,
+      e.filterExample,
+      e.foldExample,
+      e.foldLeftExample,
+      e.foldRightExample
+    )
+      .map(pair => pair._1 + " = " + pair._2.toString)
+      .mkString("\n")
+    println(allExamples)
+  }
+
+}
