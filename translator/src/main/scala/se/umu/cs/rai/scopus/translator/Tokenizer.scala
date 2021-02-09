f04bb37c15b257e89da6f3980a67dda390b62887
Julian Mendez <julian.mendez@gmail.com>
2021-02-09 14:51:23 +0100

    Make small refactoring in Tokenizer

--- translator/src/main/scala/se/umu/cs/rai/scopus/translator/Tokenizer.scala
+++ translator/src/main/scala/se/umu/cs/rai/scopus/translator/Tokenizer.scala
@@ -15,6 +15,19 @@ case class Tokenizer() {
     tokenizeRec(line, 0, 0, ParserState().Plain, Seq())
       .reverse
 
+  def _nextValues (newParserState: Int, line: String, lastIndex: Int, currentIndex: Int, parserState: Int, revTokens: Seq[Token]) =
+    if ( newParserState == parserState
+    ) (lastIndex, currentIndex + 1, revTokens)
+    else {
+      val index =
+        if ( parserState == ParserState().QuotesState || parserState == ParserState().ApostropheState
+        ) currentIndex + 1
+        else currentIndex
+
+      val text = line.substring(lastIndex, index)
+      (index, index + 1, revTokens.prepended(Token(text, parserState, lastIndex)))
+    }
+
   @tailrec final
   def tokenizeRec(line: String, lastIndex: Int, currentIndex: Int, parserState: Int, revTokens: Seq[Token]): Seq[Token] =
     if ( currentIndex >= line.length
@@ -22,19 +35,8 @@ case class Tokenizer() {
     else {
       val ch = line.charAt(currentIndex)
       val charType = CharType().charType(ch)
-      val newParserState = ParserTransition().nextParserState(parserState = parserState, charType = charType)
-      val (newLastIndex, newCurrentIndex, newRevTokens) =
-        if ( newParserState == parserState
-        ) (lastIndex, currentIndex + 1, revTokens)
-        else {
-          val index =
-            if ( parserState == ParserState().QuotesState || parserState == ParserState().ApostropheState
-            ) currentIndex + 1
-            else currentIndex
-
-          val text = line.substring(lastIndex, index)
-          (index, index + 1, revTokens.prepended(Token(text, parserState, lastIndex)))
-        }
+      val newParserState = ParserTransition().nextParserState(parserState, charType)
+      val (newLastIndex, newCurrentIndex, newRevTokens) = _nextValues (newParserState, line, lastIndex, currentIndex, parserState, revTokens)
       tokenizeRec(line, newLastIndex, newCurrentIndex, newParserState, newRevTokens)
     }
 
