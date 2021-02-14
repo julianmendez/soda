c4db9f882170acc4cb478635fa957cb2eb4b01b7
Julian Mendez <julian.mendez@gmail.com>
2021-02-14 11:45:07 +0100

    Add directory scanner

---
+++ translator/src/main/scala/se/umu/cs/rai/scopus/translator/DirectoryScanner.scala
@@ -0,0 +1,34 @@
+package se.umu.cs.rai.scopus.translator
+
+import java.io.File
+import scala.annotation.tailrec
+
+case class DirectoryScanner() {
+
+  val Scopus_suffix = ".scopus"
+
+  @tailrec final
+  def scan(found: Seq[File], to_scan: Seq[File]): Seq[File] =
+    if ( to_scan.isEmpty
+    ) found
+    else {
+      val next_file = to_scan.head
+      val remaining = to_scan.tail
+      val new_to_scan =
+        if ( next_file.isDirectory
+        ) remaining.concat(next_file.listFiles())
+        else remaining
+      scan(found.prepended(next_file), new_to_scan)
+    }
+
+  def get_all_files(start: File): Seq[File] =
+    if ( start.isFile
+    ) Seq(start)
+    else scan(Seq(), start.listFiles().toSeq)
+
+  def get_scopus_files(start:File): Seq[File] =
+    get_all_files(start)
+      .filter(x => x.isFile)
+      .filter(x => x.getName.endsWith(Scopus_suffix))
+
+}
---
+++ translator/src/test/resources/example/Example.md
@@ -0,0 +1,3 @@
+This file is not included in the scan.
+
+
