407680f602f5c8e8f8cdd04bc8449fa832414b0c
Julian Mendez <julian.mendez@gmail.com>
2021-03-05 01:46:12 +0100

    Improve unit test

--- translator/src/test/scala/scopus/translator/TokenizerSpec.scala
+++ translator/src/test/scala/scopus/translator/TokenizerSpec.scala
@@ -7,34 +7,50 @@ import scala.language.implicitConversions
 
 case class TokenizerSpec() extends AnyFunSuite {
 
-
-  val InputLine0: String = "    val Constant = \"my text\""
-  val InputLine1: String = "def f(x: Int): Int = x"
-  val InputLine2: String = "\tas_digits (5 * number)"
-
-
-  val ExpectedTokens0: Seq[Token] = Seq(
-    Token("    val Constant = ", ParserState().Plain, 0),    Token("\"my text\"", ParserState().QuotesState, 19),    Token("", ParserState().Plain, 28)
-  )
-
-  val ExpectedTokens1: Seq[Token] = Seq(
-    Token("def f(x: Int): Int = x", ParserState().Plain, 0)
-  )
-
-  val ExpectedTokens2: Seq[Token] = Seq(
-    Token("\tas_digits (5 * number)", ParserState().Plain, 0)
-  )
-
-
   test("should tokenize a small example") {
-    val obtained0 = Tokenizer().tokenize(InputLine0)
-    assert(obtained0 == ExpectedTokens0)
-
-    val obtained1 = Tokenizer().tokenize(InputLine1)
-    assert(obtained1 == ExpectedTokens1)
-
-    val obtained2 = Tokenizer().tokenize(InputLine2)
-    assert(obtained2 == ExpectedTokens2)
+    val input = "    val Constant = \"my text\""
+    val expected = Seq(
+      Token("    val Constant = ", ParserState().Plain, 0),      Token("\"my text\"", ParserState().QuotesState, 19),      Token("", ParserState().Plain, 28)
+    )
+    val obtained = Tokenizer().tokenize(input)
+    assert(obtained == expected)
   }
+
+  test("should tokenize a common tab in a string") {
+    val input = "  x = \"abc\tde\""
+    val expected = Seq(
+      Token("  x = ", ParserState().Plain, 0),      Token("\"abc\tde\"", ParserState().QuotesState, 6),      Token("", ParserState().Plain, 14)
+    )
+    val obtained = Tokenizer().tokenize(input)
+    assert(obtained == expected)
+  }
+
+  test("should tokenize an escaped tab in a string") {
+    val input = "  x = \"abc\\tde\""
+    val expected = Seq(
+      Token("  x = ", ParserState().Plain, 0),      Token("\"abc\\tde\"", ParserState().QuotesState, 6),      Token("", ParserState().Plain, 15)
+    )
+    val obtained = Tokenizer().tokenize(input)
+    assert(obtained == expected)
+  }
+
+  test("should tokenize a single function definition") {
+    val input = "def f(x: Int): Int = x"
+    val expected = Seq(
+      Token("def f(x: Int): Int = x", ParserState().Plain, 0)
+    )
+    val obtained = Tokenizer().tokenize(input)
+    assert(obtained == expected)
+  }
+
+  test("should tokenize a function call") {
+    val input = "\tas_digits (5 * number)"
+    val expected = Seq(
+      Token("\tas_digits (5 * number)", ParserState().Plain, 0)
+    )
+    val obtained = Tokenizer().tokenize(input)
+    assert(obtained == expected)
+  }
+
 
 }
