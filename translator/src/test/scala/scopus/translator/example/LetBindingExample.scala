7255b39a44f19382dcd7e3acb4dacc767f629c2b
Julian Mendez <julian.mendez@gmail.com>
2021-03-15 16:26:47 +0100

    Add examples of let binding

---
+++ translator/src/test/scala/scopus/translator/example/LetBindingExample.scala
@@ -0,0 +1,26 @@
+package scopus.translator.example
+
+
+case class LetBindingExample() {
+
+  lazy val three_parts_like_where = {
+    lazy val x = Seq(first_part, second_part, third_part)
+    /* where */
+    lazy val first_part = "first" + part
+    lazy val second_part = "second" + part
+    lazy val third_part = "third" + part
+    lazy val part = " part"
+    x
+  }
+
+  lazy val three_parts_like_let_in = {
+    /* let */
+    lazy val first_part = "first" + part
+    lazy val second_part = "second" + part
+    lazy val third_part = "third" + part
+    lazy val part = " part"
+    /* in */
+    Seq(first_part, second_part, third_part)
+  }
+
+}
---
+++ translator/src/test/scala/scopus/translator/example/LetBindingExampleSpec.scala
@@ -0,0 +1,20 @@
+package scopus.translator.example
+
+import org.scalatest.funsuite.AnyFunSuite
+
+
+case class LetBindingSpec() extends AnyFunSuite {
+
+  test("should evaluate an expression like 'where'") {
+    lazy val obtained = LetBindingExample().three_parts_like_where
+    lazy val expected = Seq("first part", "second part", "third part")
+    assert(obtained == expected)
+  }
+
+  test("should evaluate an expression like 'let in'") {
+    lazy val obtained = LetBindingExample().three_parts_like_let_in
+    lazy val expected = Seq("first part", "second part", "third part")
+    assert(obtained == expected)
+  }
+
+}
