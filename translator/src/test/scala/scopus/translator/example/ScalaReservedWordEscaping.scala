7c5d90867d8551138817439b8f919b003caf6a3d
Julian Mendez <julian.mendez@gmail.com>
2021-03-24 03:37:36 +0100

    Add unit tests

---
+++ translator/src/test/scala/scopus/translator/example/ScalaReservedWordEscaping.scala
@@ -0,0 +1,17 @@
+package scopus.translator.example
+
+case class ScalaReservedWordEscaping (  ) {
+
+  lazy val __scopus__var = "var"
+
+  lazy val __scopus__val = 1
+
+  def __scopus__def [ A , B ]  ( key: A , value: B ) : ( A , B ) = ( key , value )
+
+  def __scopus__while [ A , B ]  ( seq: Seq [ A ]  , cond: A => Boolean , __scopus__do : A => B ) =
+    seq.takeWhile ( cond ) .map ( __scopus__do )
+
+  lazy val __scopus__protected = "protected"
+  lazy val __scopus__private = "private"
+
+}
