00dffd7f721490e6a203530aeb0575fb0f7d7339
Julian Mendez <julian.mendez@gmail.com>
2021-04-03 09:28:55 +0200

    Update unit test

--- translator/src/test/scala/soda/translator/example/ScalaReservedWordEscaping.scala
+++ translator/src/test/scala/soda/translator/example/ScalaReservedWordEscaping.scala
@@ -2,16 +2,16 @@ package soda.translator.example
 
 case class ScalaReservedWordEscaping (  ) {
 
-  lazy val __scopus__var = "var"
+  lazy val __soda__var = "var"
 
-  lazy val __scopus__val = 1
+  lazy val __soda__val = 1
 
-  def __scopus__def [ A , B ]  ( key: A , value: B ) : ( A , B ) = ( key , value )
+  def __soda__def [ A , B ]  ( key: A , value: B ) : ( A , B ) = ( key , value )
 
-  def __scopus__while [ A , B ]  ( seq: Seq [ A ]  , cond: A => Boolean , __scopus__do : A => B ) =
-    seq.takeWhile ( cond ) .map ( __scopus__do )
+  def __soda__while [ A , B ]  ( seq: Seq [ A ]  , cond: A => Boolean , __soda__do : A => B ) =
+    seq.takeWhile ( cond ) .map ( __soda__do )
 
-  lazy val __scopus__protected = "protected"
-  lazy val __scopus__private = "private"
+  lazy val __soda__protected = "protected"
+  lazy val __soda__private = "private"
 
 }
