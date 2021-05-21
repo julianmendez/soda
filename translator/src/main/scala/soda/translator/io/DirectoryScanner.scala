c0fcf6dea2956b6d2d64a56f74f3d0e4cf5ad8be
Julian Mendez <julian.mendez@gmail.com>
2021-05-21 16:50:11 +0200

    Refactor directory scanner

--- translator/src/main/scala/soda/translator/io/DirectoryScanner.scala
+++ translator/src/main/scala/soda/translator/io/DirectoryScanner.scala
@@ -10,24 +10,28 @@ case class DirectoryScanner () {
   def get_all_files (start: File ): Seq [File] =
     if (start.isFile
     ) Seq (start )
-    else scan (Seq (), start.listFiles () .toSeq )
+    else RecursiveScanner (Seq (), start.listFiles () .toSeq ) .scan ()
 
-  def scan (found: Seq [File], to_scan: Seq [File]  ): Seq [File] = {
-    lazy val result = rec (found, to_scan )
+  case class RecursiveScanner (found: Seq [File], to_scan: Seq [File]  ) {
 
-    import scala.annotation.tailrec
+    def scan (): Seq [File] = {
+      lazy val result = rec (found, to_scan )
+
+      import scala.annotation.tailrec
         @tailrec
-    def rec (found: Seq [File], to_scan: Seq [File]  ): Seq [File] =
-      if (to_scan.isEmpty
-      ) found
-      else rec (found.+: (to_scan.head ), new_to_scan (to_scan )  )
+      def rec (found: Seq [File], to_scan: Seq [File]  ): Seq [File] =
+        if (to_scan.isEmpty
+        ) found
+        else rec (found.+: (to_scan.head ), get_files_to_scan (to_scan )  )
 
-    def new_to_scan (to_scan: Seq [File]  ): Seq [File] =
+      result
+    }
+
+    def get_files_to_scan (to_scan: Seq [File]  ): Seq [File] =
       if (to_scan.head.isDirectory
       ) to_scan.tail.++ (to_scan.head.listFiles ()  )
       else to_scan.tail
 
-    result
   }
 
 }
