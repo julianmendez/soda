272fb96d945ca7de409040eb198b9039193d1aea
Julian Mendez <julian.mendez@gmail.com>
2021-07-02 16:19:56 +0200

    Update unit test

--- translator/src/test/scala/soda/translator/language/LineJoinerSpec.scala
+++ translator/src/test/scala/soda/translator/language/LineJoinerSpec.scala
@@ -17,6 +17,9 @@ case class LineJoinerSpec () extends org.scalatest.funsuite.AnyFunSuite {
   lazy val Result_with_opening_closing_brackets_example =
     Seq ("a[ T ]( x: Int, y: Int ) = f[T](x + y)"    )
 
+  lazy val Example_using_extends_and_with =
+    Seq ("it should join lines ending with the extends", "keyword", "it should join lines ending with the with", "keyword", "it should join lines starting with the", "extends keyword", "it should join lines starting with the", "with keyword", "it should not join lines ending with suffixextends", "it should not join lines ending with suffixwith", "extendsprefix should not join lines", "withprefix should not join lines", "trailing spaces using extends  ", "and using with ", "should be respected", "leading spaces using", " extends and using ", "  with should be respected"    )
+
   test ("should join lines ending in comma") {
     lazy val input = Seq ("this ", "should join,", "all the lines , ", "  that end with a", "comma.")
     lazy val expected = Seq ("this ", "should join,all the lines ,   that end with a", "comma.")
@@ -53,18 +56,20 @@ case class LineJoinerSpec () extends org.scalatest.funsuite.AnyFunSuite {
     assert (obtained == expected )
   }
 
-  test ("should join lines starting or ending with keyword 'extends'") {
-    lazy val input = Seq ("this should join the lines ending with the extends", "keyword", "this should join the lines starting with the", " extends keyword", "but not if theextends", "keyword is a suffix", "or if the", "extendskeyword is a prefix"    )
-    lazy val expected = Seq ("this should join the lines ending with the extends keyword", "this should join the lines starting with the extends keyword", "but not if theextends", "keyword is a suffix", "or if the", "extendskeyword is a prefix"    )
+  test ("should join lines starting or ending with keyword 'extends' and 'with' with forward join") {
+    lazy val input = Example_using_extends_and_with
+    lazy val expected =
+      Seq ("it should join lines ending with the extendskeyword", "it should join lines ending with the withkeyword", "it should join lines starting with the", "extends keyword", "it should join lines starting with the", "with keyword", "it should not join lines ending with suffixextends", "it should not join lines ending with suffixwith", "extendsprefix should not join lines", "withprefix should not join lines", "trailing spaces using extends  and using with should be respected", "leading spaces using", " extends and using ", "  with should be respected"      )
     lazy val obtained = mt.join_lines_with_forward_join (input )
 
     assert (obtained == expected )
   }
 
-  test ("should join lines starting or ending with keyword 'with'") {
-    lazy val input = Seq ("this should join the lines ending with the with", "keyword", "this should join the lines starting with the", "with keyword", "but not if thewith", "keyword is a suffix", "or if the", "withkeyword is a prefix."    )
-    lazy val expected = Seq ("this should join the lines ending with the with keyword", "this should join the lines starting with the with keyword", "but not if thewith", "keyword is a suffix", "or if the", "withkeyword is a prefix."    )
-    lazy val obtained = mt.join_lines_with_forward_join (input )
+  test ("should join lines starting or ending with keyword 'extends' and 'with' with backward join") {
+    lazy val input = Example_using_extends_and_with
+    lazy val expected =
+      Seq ("it should join lines ending with the extends", "keyword", "it should join lines ending with the with", "keyword", "it should join lines starting with theextends keyword", "it should join lines starting with thewith keyword", "it should not join lines ending with suffixextends", "it should not join lines ending with suffixwith", "extendsprefix should not join lines", "withprefix should not join lines", "trailing spaces using extends  ", "and using with ", "should be respected", "leading spaces using extends and using   with should be respected"      )
+    lazy val obtained = mt.join_lines_with_backward_join (input )
 
     assert (obtained == expected )
   }
