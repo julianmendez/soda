224d76b2b7f2f5f096ca9eb0c9a5f03fabbfefc1
Julian Mendez <julian.mendez@gmail.com>
2021-07-02 14:32:22 +0200

    Update example

--- translator/src/test/scala/soda/translator/example/SwapExample.scala
+++ translator/src/test/scala/soda/translator/example/SwapExample.scala
@@ -4,6 +4,6 @@ case class PairExample (left: Int, right: Int )
 
 case class SwapExample () {
 
-  def swap (pair: PairExample ) =
-    Tuple2 (pair.right, pair.left )
+  def swap (pair: PairExample ): PairExample =
+    PairExample (pair.right, pair.left )
 }
