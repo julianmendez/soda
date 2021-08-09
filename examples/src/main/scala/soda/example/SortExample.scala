c5d24441671dc97d9f16a7228c1ebaeebc013ab2
Julian Mendez <julian.mendez@gmail.com>
2021-08-09 14:01:51 +0200

    Update example

--- examples/src/main/scala/soda/example/SortExample.scala
+++ examples/src/main/scala/soda/example/SortExample.scala
@@ -88,19 +88,26 @@ case class EmptySortedSequence_ [A <: Comparable [A]]  ()  extends EmptySortedSe
 
 trait NonEmptySortedSequence [A <: Comparable [A]]  extends SortedSequence [A] {
 
-  def _remaining_sequence: Seq [A]
+  def _remaining: Seq [A]
 
-  def _some_element: A
+  def _element: A
+
+  def is_less_than (x: A, y: A ): Boolean =
+    x.compareTo (y ) < 0
 
   lazy val sequence =
     {
-      lazy val first_part = _remaining_sequence.takeWhile (x => x.compareTo (_some_element ) < 0 )
-      lazy val middle = Seq (_some_element )
-      lazy val last_part = _remaining_sequence.dropWhile (x => x.compareTo (_some_element ) < 0 )
+      lazy val first_part =
+        _remaining.takeWhile (x => is_less_than (x, _element )  )
+
+      lazy val middle = Seq (_element )
+
+      lazy val last_part =
+        _remaining.dropWhile (x => is_less_than (x, _element )  )
       first_part.++ (middle.++ (last_part )  ) }
 }
 
-case class _NonEmptySortedSequence_ [A <: Comparable [A]]  (_some_element: A, _remaining_sequence: Seq [A]  )  extends NonEmptySortedSequence [A]
+case class _NonEmptySortedSequence_ [A <: Comparable [A]]  (_element: A, _remaining: Seq [A]  )  extends NonEmptySortedSequence [A]
 
 trait SortedSequenceBuilder [A <: Comparable [A]] {
   import soda.lib.Rec
@@ -111,7 +118,7 @@ trait SortedSequenceBuilder [A <: Comparable [A]] {
   lazy val _initial_value = EmptySortedSequence_ [A]  ()
 
   def _next_value_function (sorted_sequence: SortedSequence [A], element: A ): SortedSequence [A] =
-    _NonEmptySortedSequence_ [A]  (_remaining_sequence = sorted_sequence.sequence, _some_element = element )
+    _NonEmptySortedSequence_ [A]  (_remaining = sorted_sequence.sequence, _element = element )
 }
 
 case class SortedSequenceBuilder_ [A <: Comparable [A]]  () extends SortedSequenceBuilder [A]
