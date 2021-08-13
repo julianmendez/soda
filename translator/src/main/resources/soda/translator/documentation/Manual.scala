b9b6470cb02103781d5d1096591615cc22d0c228
Julian Mendez <julian.mendez@gmail.com>
2021-08-13 08:20:13 +0200

    Update tutorial

--- translator/src/main/resources/soda/translator/documentation/Manual.scala
+++ translator/src/main/resources/soda/translator/documentation/Manual.scala
@@ -6,32 +6,67 @@ package soda.translator.documentation
  * Version: 2021-08-12
  */
 
+/* To declare a class, just add `class` before a class name.
+ * It is recommended to use camel case style starting with a capital letter.
+ * The name could be a noun or an adjective, but it should not be a verb. */
 trait Shape
 
+/* There is an abbreviation for class declaration.
+ * For this, just start a line with an asterisk (`*`). */
 trait Movable
 
+/* A class can be parameterized.
+ * The parameter type can be constrained using `subtype` and `supertype`.
+ * For example, `A subtype B` means that `A` is a subtype of `B`. */
 trait ShapePainter [A <: Shape]
 
+/* It is possible to constrain a class parameter with more than one type.
+ * Connect them using `with`. */
 trait ShapeMover [A <: Shape with Movable]
 
+/* The body of a class is declared between braces (`{` and `}`) after the equals sign (`=`).
+ * It is recommended to indent the constants and functions declared inside. */
 trait EqualsExample {
 
-  lazy val answer = f (x = 20, y = 2 )
+  /* A constant does not have parameters, and it is declared with the equals sign (`=`).
+   * It is recommended to use snake case and start in lowercase.
+   * The constant name should be a noun.
+   * In a function call the parameters can be specified with the colon-equals sign (`:=`).
+   * This is especially recommended when the parameters are of the same type.
+   * Constants are only evaluated once, which is the first time they are needed. */
+  lazy val answer: Int = f (x = 20, y = 2 )
 
-  def f (x: Int, y: Int ) = 2 * x + y
+  /* A function has parameters.
+   * If the parameters are empty, it is implied that the function produces some side effect.
+   * Functions, even with empty parameters, are evaluated every time they are invoked. */
+  def f (x: Int, y: Int ): Int = 2 * x + y
 }
 
+/* A class can extend another one by using `extends`.
+ * Abstract classes cannot be instantiated but can be extended.
+ * Conversely, concrete classes cannot be extended but can be instantiated.
+ * Concrete classes are declared with parentheses `(` and `)`.
+ * It is recommended that concrete classes do not have a body, because this cannot be reused.
+ * Concrete classes extending only one class should be named as its superclass, but ending with an underscore (`_`). */
+case class EqualsExample_ () extends EqualsExample
+
+/* A class does not need to declare the all its constants and functions. */
 trait RegisteredPerson {
 
+  /* A line starting with `has` denotes a constant or function that needs to be declared in extending classes. */
   def first_name: String
 
   def last_name: String
 
+  /* If a constant or function is not meant to be exported, its name should start with an underscore (`_`). */
   lazy val _separator = " "
 
+  /* Strings can be concatenated by using the plus sign (`+`). */
   lazy val full_name = first_name + _separator + last_name
 }
 
+/* A concrete class can be declared with parameters.
+ * It is not recommended to use this pattern when using this class as receiving parameters in functions. */
 case class Person (name: String )
 
 trait Agent {
@@ -39,6 +74,8 @@ trait Agent {
   def identifier: String
 }
 
+/* A concrete class needs as parameters all the constants and functions that have not be defined in its super class.
+ * Please note that an abstract class might have constants and functions that are not defined in its ancestor classes. */
 case class Agent_ (identifier: String ) extends Agent
 
 trait RankedIndividual {
@@ -60,6 +97,7 @@ trait Visitor {
 
 case class Item (identifier: Int ) extends Element {
 
+  /* It is possible to refer to an object instance by using `this`. */
   def accept (v: Visitor ) = v.visit (this )
 }
 
@@ -67,6 +105,8 @@ trait PersonName {
 
   def name: String
 
+  /* It is possible to override a function by using annotation `@override`.
+   * This is intended only for exceptional cases, like the `toString` function. */
   override
   lazy val toString = name
 }
@@ -81,18 +121,25 @@ trait Manual {
 
   lazy val b: Int = 2
 
+  /* An instance of a JVM class can be created with annotation `@new`.
+   * If the code is translated to Scala 3, this annotation is not required. */
   lazy val now = new Date ()
 
   def plus_one (x: Int ): Int = x + 1
 
+  /* A function can be defined using a `if`-`then`-`else` structure.
+   * The condition in the `if` is evaluated, and then only the corresponding branch is evaluated. */
   def max (x: Int, y: Int ): Int =
     if (x > y
     ) x
     else y
 
+  /* Scala sequences (`Seq`) can be used, as well as other basic Scala classes.
+   * Lambda functions are declared using a right arrow (`->`). */
   def plus_one (sequence: Seq [Int]  ): Seq [Int] =
     sequence.map (element => element + 1 )
 
+  /* Boolean values `false` and `true` are available. */
   def my_not (x: Boolean ): Boolean =
     if (x
     ) false
@@ -114,13 +161,20 @@ trait Manual {
       ) true
       else false
 
+  /* Boolean values have the standard `not`-`and`-`or` functions. */
   def my_xor (x: Boolean, y: Boolean ) =
     (x || y ) && ! (x && y )
 
+  /* To evaluate a constant or a function, it is possible to declare intermediate functions.
+   * This is done in a block `let`-`in`.
+   * The block starts with a `let` containing the intermediate functions in no particular order.
+   * This block is evaluated in an expression after the `in`. */
   def sum (n: Int ) =
     {
       lazy val result = rec (n, 0 )
 
+      /* A tail recursive function should be declared inside another function.
+       * Annotation `@tailrec` helps ensuring that the tail recursion is detected and optimized. */
       import scala.annotation.tailrec
         @tailrec
       def rec (n: Int, accum: Int ): Int =
@@ -136,6 +190,8 @@ trait Manual {
       lazy val b = g (a )
       a + b }
 
+  /* Since the order in a `let`-`in` block is not important, a constant `result` at the beginning could easily indicate
+   * what is the main result to be evaluated. */
   def f1 (x: Int ): Int =
     {
       lazy val result = a + b
@@ -160,6 +216,7 @@ trait FactorialConcise  extends AbstractFactorialConcise {
       import scala.annotation.tailrec
         @tailrec
       def rec (n: Int, product: Int ): Int =
+        /* The function to compare equality is a long equals (`==`). */
         if (n == 0
         ) product
         else rec (n - 1, n * product )
@@ -167,6 +224,7 @@ trait FactorialConcise  extends AbstractFactorialConcise {
       result }
 }
 
+/* The word `is` is a synonym for the equals sign (`=`) and they are interchangeable. */
 trait AbstractFactorialVerbose {
 
   def factorial (n: Int ): Int
@@ -218,14 +276,21 @@ trait Recursion {
 
 case class Recursion_ () extends Recursion
 
+
+/* The main class is concrete and it is called `Main()`. */
+case class Main () extends MainClass
+
+/* The main class requires a `main` function that receives an `Array[String]` and returns a `Unit`. */
 trait MainClass {
 
   def main (arguments: Array [String]  ): Unit =
+    /* An output to the standard output can be send with a `println` command.
+     * This is a shorter form of JVM's `System.out.println`. */
     println ("Hello world!")
 }
 
-case class Main () extends MainClass
-
+/* The main class needs to be indicated with annotation `@main`.
+ * Only one main class per package is allowed. */
 object EntryPoint {
   def main(args: Array[String]): Unit = Main().main(args)
 }
