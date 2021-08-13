0e68ace1e70dc6d7f8b8c935471350fd83ac87e3
Julian Mendez <julian.mendez@gmail.com>
2021-08-13 10:11:09 +0200

    Update unit test

--- translator/src/test/scala/soda/translator/language/SpecificTranslationSpec.scala
+++ translator/src/test/scala/soda/translator/language/SpecificTranslationSpec.scala
@@ -34,6 +34,50 @@ trait SpecificTranslationSpec  extends org.scalatest.funsuite.AnyFunSuite {
 
     assert (obtained == expected )
   }
+
+  test ("should translate classes") {
+    lazy val original =
+      "* A =" +
+      "\n" +
+      "\n  f(x: Int): Int = x + 1" +
+      "\n" +
+      "\nclass B = {" +
+      "\n" +
+      "\n  g(x: Int): Int = 2 * x" +
+      "\n}" +
+      "\n" +
+      "\nclass C() extends A =" +
+      "\n" +
+      "\n  h(x: Int): Int = 2 * x + 1" +
+      "\n" +
+      "\n* B[T] = {" +
+      "\n" +
+      "\n  i(x: T): T = x" +
+      "\n}" +
+      "\n"
+    lazy val expected =
+      "trait A:" +
+      "\n" +
+      "\n  def f (x: Int ): Int = x + 1" +
+      "\n" +
+      "\ntrait B {" +
+      "\n" +
+      "\n  def g (x: Int ): Int = 2 * x" +
+      "\n}" +
+      "\n" +
+      "\ncase class C () extends A:" +
+      "\n" +
+      "\n  def h (x: Int ): Int = 2 * x + 1" +
+      "\n" +
+      "\ntrait B [T] {" +
+      "\n" +
+      "\n  def i (x: T ): T = x" +
+      "\n}" +
+      "\n"
+    lazy val obtained = MicroTranslator_ () .translate_program (original )
+
+    assert (obtained == expected )
+  }
 }
 
 case class SpecificTranslationSpec_ () extends SpecificTranslationSpec
