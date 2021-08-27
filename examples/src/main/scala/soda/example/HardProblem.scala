27ebb3ee3bab7961a0472b0098f7e9f10a62ae38
Julian Mendez <julian.mendez@gmail.com>
2021-08-27 15:06:09 +0200

    Refactor HardProblem

--- examples/src/main/scala/soda/example/HardProblem.scala
+++ examples/src/main/scala/soda/example/HardProblem.scala
@@ -1,8 +1,13 @@
 package soda.translator.example
 
-trait Memoizer [A, B] {
+trait MemoizableFunction [A, B] {
 
-  def main_function (x: A, memoized_values: Map [A, B]  ): Tuple2 [B, Map [A, B]]
+  def compute (x: A, memoized_values: Map [A, B]  ): Tuple2 [B, Map [A, B]]
+}
+
+trait Memoizer [A, B] extends MemoizableFunction [A, B] {
+
+  def main_function: (x: A, memoized_values: Map [A, B]  ) => Tuple2 [B, Map [A, B]]
 
   def compute (x: A, memoized_values: Map [A, B]  ): Tuple2 [B, Map [A, B]] =
     {
@@ -20,7 +25,11 @@ trait Memoizer [A, B] {
       (res, map + new_pair ) }
 }
 
-trait HardProblem  extends Memoizer [Int, Int] {
+case class Memoizer_ [A, B]  (main_function: (x: A, memoized_values: Map [A, B]  ) => Tuple2 [B, Map [A, B]]  ) extends Memoizer [A, B]
+
+trait HardProblem  extends MemoizableFunction [Int, Int] {
+
+  lazy val memoizer = Memoizer_ [Int, Int]  (main_function )
 
   def is_even (n: Int ): Boolean =
     n % 2 == 0
@@ -37,11 +46,16 @@ trait HardProblem  extends Memoizer [Int, Int] {
       {
         lazy val (res, new_map ) = compute (one_step (n ), memoized_values )
         (1 + res, new_map ) }
+
+  def compute (n: Int, memoized_values: Map [Int, Int]  ): Tuple2 [Int, Map [Int, Int]] =
+    memoizer.compute (n, memoized_values )
 }
 
 case class HardProblem_ () extends HardProblem
 
-trait MemoizedFibonacci  extends Memoizer [Int, Int] {
+trait MemoizedFibonacci  extends MemoizableFunction [Int, Int] {
+
+  lazy val memoizer = Memoizer_ [Int, Int]  (main_function )
 
   def main_function (n: Int, memoized_values: Map [Int, Int]  ): Tuple2 [Int, Map [Int, Int]] =
     if (n == 0 ) (0, memoized_values )
@@ -53,6 +67,9 @@ trait MemoizedFibonacci  extends Memoizer [Int, Int] {
         lazy val res = res1 + res2
         lazy val new_pair = (n, res )
         (res, map2 + new_pair ) }
+
+  def compute (n: Int, memoized_values: Map [Int, Int]  ): Tuple2 [Int, Map [Int, Int]] =
+    memoizer.compute (n, memoized_values )
 }
 
 case class MemoizedFibonacci_ () extends MemoizedFibonacci
