cd3a2cc8663b747b1294366bda07a98b7eccdfbc
Julian Mendez <julian.mendez@gmail.com>
2021-09-10 18:04:26 +0200

    Add first preprocessing for Coq port

--- coqport/src/test/scala/soda/coqport/language/MicroTranslatorSpec.scala
+++ coqport/src/test/scala/soda/coqport/language/MicroTranslatorSpec.scala
@@ -5,7 +5,7 @@ case class MicroTranslatorSpec ()  extends org.scalatest.funsuite.AnyFunSuite {
 
   test ("Coq translation") {
     lazy val original = "x = 0"
-    lazy val expected = "x = 0"
+    lazy val expected = "x = 0\n"
     /* FIXME */
 
     lazy val obtained = MicroTranslator_ () .translate_program (original )
