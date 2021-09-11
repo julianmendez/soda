23af55be30d52e751e9afd2c88e84ad9d74f0b19
Julian Mendez <julian.mendez@gmail.com>
2021-09-12 01:02:10 +0200

    Make minor change

--- examples/src/main/scala/soda/example/PatternMatching.scala
+++ examples/src/main/scala/soda/example/PatternMatching.scala
@@ -8,23 +8,21 @@ trait Parameter {
 
 trait PatternMatching {
 
-  def get_value (p: Parameter ): Int = {
+  def get_value (p: Parameter ): Int =
     p match {
       case Singleton (x ) => x
       case Pair (x, y ) => (x + y ) / 2
       case Triplet (x, y, z ) => (x + y + z ) / 3
       case otherwise => 0
     }
-  }
 
-  def get_type_name (p: Parameter ): String = {
+  def get_type_name (p: Parameter ): String =
     p match {
       case x: Singleton => x.name + "(x)"
       case x: Pair => x.name + "(x, y)"
       case x: Triplet => x.name + "(x, y, z)"
       case otherwise => ""
     }
-  }
 }
 
 case class PatternMatching_ ()  extends PatternMatching
