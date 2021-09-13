542ce9543d382274f17fe72c1caa609d2cc80525
Julian Mendez <julian.mendez@gmail.com>
2021-09-13 17:57:35 +0200

    Add example in Coq

---
+++ examples/src/main/coq/soda/example/SwapExample.v
@@ -0,0 +1,51 @@
+(* package soda.example *)
+
+
+Module soda_example_SwapExample.
+
+Inductive PairExample :=
+  | PairExample_ (x: nat) (y: nat) .
+
+Definition left (pair: PairExample) :=
+  match pair with
+    | (PairExample_ x y) => x
+  end.
+
+Definition right (pair: PairExample) :=
+  match pair with
+    | (PairExample_ x y) => y
+  end.
+
+Definition swap (pair: PairExample): PairExample :=
+  (PairExample_ (right pair) (left pair)).
+
+
+Lemma swap_of_swap : forall (pair: PairExample), swap (swap pair) = pair.
+Proof.
+  intros p.
+  destruct p.
+  compute.
+  apply eq_refl.
+Qed.
+
+End soda_example_SwapExample.
+
+
+Import soda_example_SwapExample.
+
+Example test_1 : swap (PairExample_ 2 3) = (PairExample_ 3 2).
+Proof.
+  compute.
+  apply eq_refl.
+Qed.
+
+
+Example test_2 : swap (swap (PairExample_ 2 3)) = (PairExample_ 2 3).
+Proof.
+  compute.
+  apply eq_refl.
+Qed.
+
+
+
+
