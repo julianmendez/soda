8f7cd1e0cd4c4cff81f83a32c288dbb76552ddca
Julian Mendez <julian.mendez@gmail.com>
2021-09-17 10:58:24 +0200

    Update example in Coq

--- examples/src/main/coq/soda/example/FactorialConcise.v
+++ examples/src/main/coq/soda/example/FactorialConcise.v
@@ -4,6 +4,9 @@
 Require Import Coq.Init.Nat.
 (* https://coq.inria.fr/library/Coq.Init.Nat.html *)
 
+Require Import Coq.Init.Datatypes.
+(* https://coq.inria.fr/library/Coq.Init.Datatypes.html *)
+
 Require Import Coq.Lists.List.
 (* https://coq.inria.fr/library/Coq.Lists.List.html *)
 
@@ -22,21 +25,8 @@ End soda_example_FactorialConcise.
 
 Import soda_example_FactorialConcise.
 
-Inductive Pair :=
-  | Pair_ (x: nat) (y: nat) .
-
-Definition fst (p: Pair): nat :=
-  match p with
-    | Pair_ x y => x
-  end.
-
-Definition snd (p: Pair): nat :=
-  match p with
-    | Pair_ x y => y
-  end.
-
-Definition factorial_values: list Pair :=
-    (Pair_ 0 1) :: (Pair_ 1 1) :: (Pair_ 2 2) :: (Pair_ 3 6) :: (Pair_ 4 24) :: (Pair_ 5 120) :: (Pair_ 6 720) :: nil.
+Definition factorial_values: list (prod nat nat) :=
+    (pair 0 1) :: (pair 1 1) :: (pair 2 2) :: (pair 3 6) :: (pair 4 24) :: (pair 5 120) :: (pair 6 720) :: nil.
 
 Example test_1 :
   map get_factorial (map fst factorial_values) = map snd factorial_values.
@@ -44,6 +34,4 @@ Proof.
   compute.
   apply eq_refl.
 Qed.
-
-
 
