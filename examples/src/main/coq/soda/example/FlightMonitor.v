11e574f66e17d77c750da73122656cd70de5c6c8
Julian Mendez <julian.mendez@gmail.com>
2021-09-22 00:12:11 +0200

    Update FlightMonitor example in Coq

--- examples/src/main/coq/soda/example/FlightMonitor.v
+++ examples/src/main/coq/soda/example/FlightMonitor.v
@@ -7,13 +7,30 @@ Require Import Coq.Init.Nat.
 Require Import Coq.Strings.String.
 (* https://coq.inria.fr/library/Coq.Strings.String.html *)
 
+Require Import Ascii.
+(* https://coq.inria.fr/library/Coq.Strings.Ascii.html *)
+
 Require Import Coq.Lists.List.
 (* https://coq.inria.fr/library/Coq.Lists.List.html *)
 
 
-Inductive Airport : Type :=
-  | Airport_ (name: string) .
+(** Date *)
+Notation Date := nat.
 
+Definition get_a_year_before (date: Date): Date :=
+  date - 365.
+
+
+(** Money *)
+Notation Money := nat.
+
+
+(** Airport *)
+Inductive Airport : Type :=
+  | Airport_ (a: ascii) (b: ascii) (c: ascii).
+
+
+(** Segment *)
 Inductive Segment : Type :=
   | Segment_ (start_airport: Airport) (end_airport: Airport).
 
@@ -27,49 +44,43 @@ Definition seg_end_airport (segment: Segment): Airport :=
     | Segment_ start_airport end_airport => end_airport
   end.
 
+
+(** Flight *)
 Inductive Flight : Type :=
-  | SingleSegmentFlight_ (segment: Segment)
-  | Flight_ (segment: Segment) (more_segments: list Segment).
+  | SingleSegmentFlight_ (start_airport: Airport) (end_airport: Airport)
+  | Flight_ (start_airport: Airport) (intermediate_stops: list Airport) (end_airport: Airport).
 
 Definition flg_start_airport (flight: Flight): Airport :=
   match flight with
-    | SingleSegmentFlight_ segment => (seg_start_airport segment)
-    | Flight_ segment more_segments => (seg_start_airport segment)
-  end.
-
-Fixpoint last_segment (head: Segment) (tail: list Segment): Segment :=
-  match tail with
-    | nil => head
-    | (cons new_head new_tail) => last_segment new_head new_tail
+    | SingleSegmentFlight_ start_airport end_airport => start_airport
+    | Flight_ start_airport intermediate_stops end_airport => start_airport
   end.
 
 Definition flg_end_airport (flight: Flight): Airport :=
   match flight with
-    | SingleSegmentFlight_ segment => (seg_end_airport segment)
-    | Flight_ segment more_segments => (seg_end_airport (last_segment segment more_segments))
+    | SingleSegmentFlight_ start_airport end_airport => end_airport
+    | Flight_ start_airport intermediate_stops end_airport => end_airport
   end.
 
-Definition is_price_increase_acceptable (old_price: nat) (new_price: nat): bool :=
-    (new_price <=? ((old_price * 125) / 100) ).
 
-Notation Date := nat.
+(** MonitoringAgent *)
 
-Definition get_a_year_before (date: Date): Date :=
-  date - 365.
+(** Rule 1 *)
 
-Notation Money := nat.
-
-Definition complies_with_ethical_rule_2 (get_price: Flight -> Date -> Money) (flight: Flight) (date: Date): bool :=
-  (is_price_increase_acceptable (get_price flight date) (get_price flight (get_a_year_before date) ) ).
+Fixpoint flg_segments_multi (start_airport: Airport) (intermediate_stops: list Airport) (end_airport: Airport): list Segment :=
+  match intermediate_stops with
+    | nil => (Segment_ start_airport end_airport) :: nil
+    | x :: xs => (Segment_ start_airport x) :: (flg_segments_multi x xs end_airport)
+  end.
 
 Definition flg_segments (flight: Flight): list Segment :=
   match flight with
-    | SingleSegmentFlight_ segment => segment :: nil
-    | Flight_ segment more_segments => segment :: more_segments
+    | SingleSegmentFlight_ start_airport end_airport => (Segment_ start_airport end_airport) :: nil
+    | Flight_ start_airport intermediate_stops end_airport => (flg_segments_multi start_airport intermediate_stops end_airport)
   end.
 
 Definition prices_of_segments (get_price : Flight -> Date -> Money) (segments: list Segment) (date: Date): list Money :=
-  map (fun segment => (get_price (SingleSegmentFlight_ segment) date) ) segments.
+  map (fun segment => (get_price (SingleSegmentFlight_ (seg_start_airport segment) (seg_end_airport segment) ) date) ) segments.
 
 Definition sum_of_prices (prices: list Money): Money :=
   fold_left (fun x => (fun y => x + y)) prices 0.
@@ -79,4 +90,13 @@ Definition price_of_flight_by_segments (get_price : Flight -> Date -> Money) (fl
 
 Definition complies_with_ethical_rule_1 (get_price : Flight -> Date -> Money) (flight: Flight) (date: Date): bool :=
   (get_price flight date) <=? (price_of_flight_by_segments get_price flight date).
+
+(** Rule 2 *)
+
+Definition is_price_increase_acceptable (old_price: nat) (new_price: nat): bool :=
+    (new_price <=? ((old_price * 125) / 100) ).
+
+Definition complies_with_ethical_rule_2 (get_price: Flight -> Date -> Money) (flight: Flight) (date: Date): bool :=
+  (is_price_increase_acceptable (get_price flight date) (get_price flight (get_a_year_before date) ) ).
+
 
