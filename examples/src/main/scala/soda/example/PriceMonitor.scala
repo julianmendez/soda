3c6a3448047cd481ef4d4064ae5a27f3b756a744
Julian Mendez <julian.mendez@gmail.com>
2021-09-28 18:48:37 +0200

    Update PriceMonitor example

--- examples/src/main/scala/soda/example/PriceMonitor.scala
+++ examples/src/main/scala/soda/example/PriceMonitor.scala
@@ -8,6 +8,15 @@ trait Customer {
   def ip_address: String
 }
 
+case class Customer_ (name: String, ip_address: String )  extends Customer
+
+trait PricingAgent {
+
+  def get_price: (Customer, Flight, Int ) => Int
+}
+
+case class PricingAgent_ (get_price: (Customer, Flight, Int ) => Int )  extends PricingAgent
+
 trait Airport {
   import soda.lib.OptionSD
   import soda.lib.SomeSD_
@@ -30,12 +39,6 @@ case class AirportBuilder () {
 
   def build (name: String ): OptionSD [Airport] =
     _Airport_ (name ) .Airport_checked
-}
-
-trait PricingAgent {
-  import java.util.Date
-
-  def get_price (customer: Customer, flight: Flight, date: Date ): Int
 }
 
 trait Trip {
@@ -71,51 +74,73 @@ trait Flight  extends Trip {
       ! (intermediate_stops.contains (airport_end )  )
 }
 
+trait FlightSocietalPrinciple {
+
+  def pricing_agent: PricingAgent
+
+  def get_price (customer: Customer, flight: Flight, date_in_days: Int ): Int =
+    pricing_agent.get_price (customer, flight, date_in_days )
+}
+
+trait FlightSocietalPrinciple1 extends FlightSocietalPrinciple {
+
+  lazy val minimum_acceptable_similarity = 0.95
+
+  def min (x: Int, y: Int ): Int = if (x < y ) x else y
+
+  def max (x: Int, y: Int ): Int = if (x > y ) x else y
+
+  def complies (c1: Customer, c2: Customer, flight: Flight, date_in_days: Int ): Boolean =
+    {
+      lazy val price_for_c1 = get_price (c1, flight, date_in_days )
+      lazy val price_for_c2 = get_price (c2, flight, date_in_days )
+      lazy val similarity = min (price_for_c1, price_for_c2 ) / max (price_for_c1, price_for_c2 )
+      similarity >= minimum_acceptable_similarity }
+}
+
+case class FlightSocietalPrinciple1_ (get_price: (Customer, Flight, Int ) => Int )
+
+trait FlightSocietalPrinciple2  extends FlightSocietalPrinciple {
+
+  lazy val acceptable_yearly_increase = 1.25
+
+  def get_a_year_before (date_in_days: Int ): Int =
+    date_in_days - 365
+
+  def complies (customer: Customer, flight: Flight, date_in_days: Int ): Boolean =
+    {
+      lazy val old_price = get_price (customer, flight, get_a_year_before (date_in_days ) )
+      lazy val new_price = get_price (customer, flight, date_in_days )
+      new_price <= old_price * acceptable_yearly_increase }
+}
+
+case class FlightSocietalPrinciple2_ (get_price: (Customer, Flight, Int ) => Int )
+
+trait FlightSocietalPrinciple3  extends FlightSocietalPrinciple {
+
+  def complies (customer: Customer, flight: Flight, date_in_days: Int ): Boolean =
+    get_price (customer, flight, date_in_days ) <= price_of_flight_by_segments (customer, flight, date_in_days )
+
+  def price_of_flight_by_segments (customer: Customer, flight: Flight, date_in_days: Int ): Int =
+    sum_of_prices (prices_of_segments (customer, flight.segments, date_in_days )  )
+
+  def prices_of_segments (customer: Customer, segments: Seq [Segment], date_in_days: Int ): Seq [Int] =
+    segments.map (segment => get_price (customer, SingleSegmentFlight_ (segment.airport_start, segment.airport_end ), date_in_days ) )
+
+  def sum_of_prices (prices: Seq [Int]  ): Int =
+    prices.sum
+}
+
+case class FlightSocietalPrinciple3_ (get_price: (Customer, Flight, Int ) => Int )
 
 trait PriceMonitorAgent {
   import java.util.Date
 
   def pricing_agent: PricingAgent
 
-  def get_a_year_before (date: Date ): Date
+  def get_a_year_before (date_in_days: Date ): Date
 
   def as_flight (segment: Segment ): Flight
-
-  def get_price (customer: Customer, flight: Flight, date: Date ): Int =
-    pricing_agent.get_price (customer, flight, date )
-
-  def min (x: Int, y: Int ): Int = if (x < y ) x else y
-
-  def max (x: Int, y: Int ): Int = if (x > y ) x else y
-
-  lazy val minimum_acceptable_similarity = 0.95
-
-  def complies_with_societal_principle_1 (customer1: Customer, customer2: Customer, flight: Flight, date: Date ): Boolean =
-    {
-      lazy val price_for_customer1 = get_price (customer1, flight, date )
-      lazy val price_for_customer2 = get_price (customer2, flight, date )
-      lazy val similarity = min (price_for_customer1, price_for_customer2 ) / max (price_for_customer1, price_for_customer2 )
-      similarity  >= minimum_acceptable_similarity }
-
-  lazy val acceptable_yearly_increase = 1.25
-
-  def complies_with_societal_principle_2 (customer: Customer, flight: Flight, date: Date ): Boolean =
-    {
-      lazy val old_price = get_price (customer, flight, get_a_year_before (date ) )
-      lazy val new_price = get_price (customer, flight, date )
-      new_price <= old_price * acceptable_yearly_increase }
-
-  def complies_with_societal_principle_3 (customer: Customer, flight: Flight, date: Date ): Boolean =
-    get_price (customer, flight, date ) <= price_of_flight_by_segments (customer, flight, date )
-
-  def price_of_flight_by_segments (customer: Customer, flight: Flight, date: Date ): Int =
-    sum_of_prices (prices_of_segments (customer, flight.segments, date )  )
-
-  def prices_of_segments (customer: Customer, segments: Seq [Segment], date: Date ): Seq [Int] =
-    segments.map (segment => get_price (customer, SingleSegmentFlight_ (segment.airport_start, segment.airport_end ), date ) )
-
-  def sum_of_prices (prices: Seq [Int]  ): Int =
-    prices.sum
 }
 
 trait SingleSegmentFlight  extends Flight {
