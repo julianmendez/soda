94a97d144cc3ed96274638b085a1213d9421a3e5
Julian Mendez <julian.mendez@gmail.com>
2021-11-09 23:29:56 +0100

    Fix unit test

--- examples/src/test/scala/soda/example/forcoq/RecursionForCoqSpec.scala
+++ examples/src/test/scala/soda/example/forcoq/RecursionForCoqSpec.scala
@@ -6,8 +6,8 @@ case class RecursionForCoqSpec ()  extends org.scalatest.funsuite.AnyFunSuite {
   lazy val exampleSeq: Seq [Int] = Seq (0, 1, 1, 2, 3, 5, 8 )
 
   test ("fold left while with Seq") {
-    lazy val initial_value = Seq ()
-    lazy val next_value_function: Seq [String] => Int => Seq [String] = (s: Seq [String]  ) => (e: Int ) => s.+: ("" + (e + 100 )  )
+    lazy val initial_value = Seq [String]  ()
+    lazy val next_value_function: Seq [String] => Int => Seq [String] = (s: Seq [String]  ) => (e: Int ) => s .+: ("" + (e + 100 )  )
     lazy val condition: Seq [String] => Int => Boolean = (s: Seq [String]  ) => (e: Int ) => e < 5
     lazy val expected = Seq ("103", "102", "101", "101", "100")
     lazy val obtained = RecursionForCoq_ () .fold4 (exampleSeq ) (initial_value ) (next_value_function ) (condition )
@@ -16,7 +16,7 @@ case class RecursionForCoqSpec ()  extends org.scalatest.funsuite.AnyFunSuite {
   }
 
   test ("fold left with Seq") {
-    lazy val initial_value = Seq ()
+    lazy val initial_value = Seq [String]  ()
     lazy val next_value_function: Seq [String] => Int => Seq [String] = (s: Seq [String]  ) => (e: Int ) => s .+: ("" + (e + 100 )  )
     lazy val expected = Seq ("108", "105", "103", "102", "101", "101", "100")
     lazy val obtained = RecursionForCoq_ () .fold3 (exampleSeq ) (initial_value ) (next_value_function )
@@ -25,21 +25,21 @@ case class RecursionForCoqSpec ()  extends org.scalatest.funsuite.AnyFunSuite {
   }
 
   test ("range with positive number") {
-    lazy val expected = Seq (0, 1, 2, 3, 4, 5, 6, 7 )
+    lazy val expected = Seq [Int]  (0, 1, 2, 3, 4, 5, 6, 7 )
     lazy val obtained = RecursionForCoq_ () .range (8 )
 
     assert (obtained == expected )
   }
 
   test ("range with zero size") {
-    lazy val expected = Seq ()
+    lazy val expected = Seq [Int]  ()
     lazy val obtained = RecursionForCoq_ () .range (-1 )
 
     assert (obtained == expected )
   }
 
   test ("range with negative number") {
-    lazy val expected = Seq ()
+    lazy val expected = Seq [Int]  ()
     lazy val obtained = RecursionForCoq_ () .range (-1 )
 
     assert (obtained == expected )
