641eb7addb41c0767b1951ef136abc2047da4a11
Julian Mendez <julian.mendez@gmail.com>
2021-12-21 21:40:48 +0100

    Refactor line joiner

--- translator/src/main/scala/soda/translator/blocktr/LineBackwardJoinerBlockTranslator.scala
+++ translator/src/main/scala/soda/translator/blocktr/LineBackwardJoinerBlockTranslator.scala
@@ -9,8 +9,29 @@ trait LineBackwardJoinerBlockTranslator  extends soda.translator.block.BlockTran
 
   lazy val target = "soda"
 
+  lazy val space = " "
+
+  lazy val reserved_word_joiner: Seq [String] = Seq ("extends", "with"  )
+
+  lazy val symbol_backward_joiner: Seq [String] = Seq (")", "]"  )
+
+
+
   def translate (block: Block ): Block =
-    Block_ (LineJoinerToScala_ (block.lines ) .joined_lines_with_backward_join    )
+    Block_ (Joiner_ (block.lines, is_a_backward_join ) .join    )
+
+  def is_a_backward_join (previous_line: String, current_line: String ): Boolean =
+    is_a_symbol_backward_join (current_line ) ||
+    is_a_reserved_word_backward_join (current_line )
+
+  def is_a_symbol_backward_join (current_line: String ): Boolean =
+    symbol_backward_joiner
+      .exists (current_line.startsWith )
+
+  def is_a_reserved_word_backward_join (current_line: String ): Boolean =
+    reserved_word_joiner
+      .map (x => x + space )
+      .exists (current_line.startsWith )
 
 }
 
--- translator/src/main/scala/soda/translator/blocktr/LineForwardJoinerBlockTranslator.scala
+++ translator/src/main/scala/soda/translator/blocktr/LineForwardJoinerBlockTranslator.scala
@@ -9,8 +9,27 @@ trait LineForwardJoinerBlockTranslator  extends soda.translator.block.BlockTrans
 
   lazy val target = "soda"
 
+  lazy val space = " "
+
+  lazy val reserved_word_joiner: Seq [String] = Seq ("extends", "with"  )
+
+  lazy val symbol_forward_joiner: Seq [String] = Seq (",", "(", "["  )
+
   def translate (block: Block ): Block =
-    Block_ (LineJoinerToScala_ (block.lines ) .joined_lines_with_forward_join    )
+    Block_ (Joiner_ (block.lines, is_a_forward_join ) .join    )
+
+  def is_a_forward_join (previous_line: String, current_line: String ): Boolean =
+    is_a_symbol_forward_join (previous_line ) ||
+    is_a_reserved_word_forward_join (previous_line )
+
+  def is_a_symbol_forward_join (previous_line: String ): Boolean =
+    symbol_forward_joiner
+      .exists (previous_line.endsWith )
+
+  def is_a_reserved_word_forward_join (previous_line: String ): Boolean =
+    reserved_word_joiner
+      .map (x => space + x )
+      .exists (previous_line.endsWith )
 
 }
 
