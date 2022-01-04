79d5cfab698847a4b353271b8dada40e5c4c8c5c
Julian Mendez <julian.mendez@gmail.com>
2022-01-04 01:18:34 +0100

    Make minor change in unit test

--- examples/src/test/scala/soda/example/mathematics/PiIteratorSpec.scala
+++ examples/src/test/scala/soda/example/mathematics/PiIteratorSpec.scala
@@ -2,12 +2,14 @@ package soda.example.mathematics
 
 case class PiIteratorSpec ()  extends org.scalatest.funsuite.AnyFunSuite {
 
-  lazy val piStart = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446"
+  lazy val pi_start = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446"
+
+  lazy val pi_sequence = PiIterator_ () .take (128 )
 
   test ("first digits of Pi")
     {
-      lazy val piSequence = PiIterator_ () .take (128 )
-      lazy val obtained = "" + piSequence.head + "." + piSequence.tail.mkString ("")
-      assert (obtained == piStart ) }
+      lazy val expected = pi_start
+      lazy val obtained = "" + pi_sequence.head + "." + pi_sequence.tail.mkString ("")
+      assert (obtained == expected ) }
 
 }
