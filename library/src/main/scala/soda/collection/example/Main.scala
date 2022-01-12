c6520ac72ff5064d75cd2c9286e384a78f726f51
Julian Mendez <julian.mendez@gmail.com>
2022-01-12 01:08:01 +0100

    Make minor change

--- library/src/main/scala/soda/collection/example/Main.scala
+++ library/src/main/scala/soda/collection/example/Main.scala
@@ -9,7 +9,8 @@ trait MainClass {
 
   lazy val e = ListExample_ ()
 
-  lazy val all_examples = Seq (e.a_example, e.b_example, e.take_example, e.takeRight_example, e.takeWhile_example, e.drop_example, e.dropRight_example, e.dropWhile_example, e.splitAt_example, e.indices_example, e.zipWithIndex_example, e.zip_example, e.reverse_example, e.prepended_example, e.appended_example, e.concat_example, e.span_example, e.map_example, e.filter_example, e.fold_example, e.foldLeft_example, e.foldRight_example  )
+  lazy val all_examples =
+    Seq (e.a_example, e.b_example, e.take_example, e.takeRight_example, e.takeWhile_example, e.drop_example, e.dropRight_example, e.dropWhile_example, e.splitAt_example, e.indices_example, e.zipWithIndex_example, e.zip_example, e.reverse_example, e.prepended_example, e.appended_example, e.concat_example, e.span_example, e.map_example, e.filter_example, e.fold_example, e.foldLeft_example, e.foldRight_example    )
     .map (pair => pair.name + " = " + pair.result.toString )
     .mkString ("\n")
 
