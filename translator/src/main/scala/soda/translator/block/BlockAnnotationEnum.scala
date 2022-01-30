0e688375d78b9132492c99e2e20a055cdaee63d9
Julian Mendez <julian.mendez@gmail.com>
2022-01-30 13:30:30 +0100

    Refactor BlockAnnotationId

--- translator/src/main/scala/soda/translator/block/BlockAnnotationEnum.scala
+++ translator/src/main/scala/soda/translator/block/BlockAnnotationEnum.scala
@@ -1,8 +1,18 @@
 package soda.translator.block
 
-case class BlockAnnotationId (ordinal: Int, name: String )
+trait BlockAnnotationId
   extends
     soda.lib.EnumConstant
+{
+
+  def   ordinal: Int
+  def   name: String
+
+}
+
+case class BlockAnnotationId_ (ordinal: Int, name: String )
+  extends
+    BlockAnnotationId
 {
 
 }
@@ -12,31 +22,31 @@ trait BlockAnnotationEnum
     soda.lib.Enum [BlockAnnotationId]
 {
 
-  lazy val undefined = BlockAnnotationId (0, "undefined")
+  lazy val undefined = BlockAnnotationId_ (0, "undefined")
 
-  lazy val function_definition = BlockAnnotationId (1, "function_definition")
+  lazy val function_definition = BlockAnnotationId_ (1, "function_definition")
 
-  lazy val class_beginning = BlockAnnotationId (2, "class_beginning")
+  lazy val class_beginning = BlockAnnotationId_ (2, "class_beginning")
 
-  lazy val class_end = BlockAnnotationId (3, "class_end")
+  lazy val class_end = BlockAnnotationId_ (3, "class_end")
 
-  lazy val class_declaration = BlockAnnotationId (4, "class_declaration")
+  lazy val class_declaration = BlockAnnotationId_ (4, "class_declaration")
 
-  lazy val abstract_block_declaration = BlockAnnotationId (5, "abstract_block_declaration")
+  lazy val abstract_block_declaration = BlockAnnotationId_ (5, "abstract_block_declaration")
 
-  lazy val abstract_function_declaration = BlockAnnotationId (6, "abstract_function_declaration")
+  lazy val abstract_function_declaration = BlockAnnotationId_ (6, "abstract_function_declaration")
 
-  lazy val import_declaration = BlockAnnotationId (7, "import_declaration")
+  lazy val import_declaration = BlockAnnotationId_ (7, "import_declaration")
 
-  lazy val package_declaration = BlockAnnotationId (8, "package_declaration")
+  lazy val package_declaration = BlockAnnotationId_ (8, "package_declaration")
 
-  lazy val theorem_block = BlockAnnotationId (9, "theorem_block")
+  lazy val theorem_block = BlockAnnotationId_ (9, "theorem_block")
 
-  lazy val proof_block = BlockAnnotationId (10, "proof_block")
+  lazy val proof_block = BlockAnnotationId_ (10, "proof_block")
 
-  lazy val comment = BlockAnnotationId (11, "comment")
+  lazy val comment = BlockAnnotationId_ (11, "comment")
 
-  lazy val test_declaration = BlockAnnotationId (12, "test_declaration")
+  lazy val test_declaration = BlockAnnotationId_ (12, "test_declaration")
 
   lazy val values =
     Seq (
