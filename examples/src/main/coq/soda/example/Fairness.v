99e04c63c2edb0b9edd8ff5e33ae9db2ab4ffeed
Julian Mendez <julian.mendez@gmail.com>
2022-02-15 19:04:51 +0100

    Update Coq examples

--- examples/src/main/coq/soda/example/Fairness.v
+++ examples/src/main/coq/soda/example/Fairness.v
@@ -15,9 +15,9 @@ Module soda_example_fairness.
 Module Applicant.
 
   Inductive type : Type :=
-    | Applicant_ (score: float).
+    | Applicant_ (score : float).
 
-  Definition score (applicant: Applicant.type): float :=
+  Definition score (applicant : Applicant.type) : float :=
     match applicant with
       | Applicant_ s => s
     end.
@@ -29,33 +29,33 @@ End Applicant.
 
 Module Fairness.
 
-  Definition score_difference_tolerance: float := 0.125 .
+  Definition score_difference_tolerance : float := 0.125 .
 
-  Definition ranking_difference_tolerance: float := 0.5 .
+  Definition ranking_difference_tolerance : float := 0.5 .
 
-  Definition rank (applicant: Applicant.type): float :=
+  Definition rank (applicant : Applicant.type) : float :=
     (Applicant.score applicant) * 2.
 
-  Definition maximum_execution_time: float := 10 .
+  Definition maximum_execution_time : float := 10 .
 
-  Definition measure_time (value: float): float := 1 .
+  Definition measure_time (value : float) : float := 1 .
 
-  Definition difference_between (value: float) (another_value: float): float :=
+  Definition difference_between (value : float) (another_value : float) : float :=
     (abs (value - another_value) ).
 
-  Definition have_similar_score (score1: float) (score2: float): bool :=
+  Definition have_similar_score (score1 : float) (score2 : float) : bool :=
     (leb (difference_between score1 score2) score_difference_tolerance ).
 
-  Definition have_similar_ranking (result1: float) (result2: float) :=
+  Definition have_similar_ranking (result1 : float) (result2 : float) :=
     (leb (difference_between result1 result2) ranking_difference_tolerance ).
 
 
-  Definition is_response_time_acceptable (applicant: Applicant.type): bool :=
+  Definition is_response_time_acceptable (applicant : Applicant.type) : bool :=
     if (leb (measure_time (rank applicant) ) maximum_execution_time )
     then true
     else false.
 
-  Definition is_fair (alice: Applicant.type) (bob: Applicant.type) :=
+  Definition is_fair (alice : Applicant.type) (bob : Applicant.type) :=
     if (have_similar_score (Applicant.score alice) (Applicant.score bob))
     then (have_similar_ranking (rank alice) (rank bob))
     else true.
