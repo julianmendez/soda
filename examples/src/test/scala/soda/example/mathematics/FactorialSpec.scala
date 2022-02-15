56d2944c086baa95014306cd422074d4895a89fa
Julian Mendez <julian.mendez@gmail.com>
2022-02-15 21:11:21 +0100

    Replace use of `let`-`in` in some unit tests

--- examples/src/test/scala/soda/example/mathematics/FactorialSpec.scala
+++ examples/src/test/scala/soda/example/mathematics/FactorialSpec.scala
@@ -5,40 +5,47 @@ case class FactorialSpec ()
     org.scalatest.funsuite.AnyFunSuite
 {
 
+  def check [A] (obtained: A ) (expected: A ): org.scalatest.compatible.Assertion =
+    assert (obtained == expected )
+
   lazy val factorial_values = Seq (
     (0, 1 ), (1, 1 ), (2, 2 ), (3, 6 ), (4, 24 ), (5, 120 ), (6, 720 ), (7, 5040 ), (8, 40320 ), (9, 362880 ), (10, 3628800 )
   )
 
-  test ("should test the factorial - concise version")
-    {
-      lazy val expected = factorial_values
-      lazy val obtained = factorial_values
+  test ("should test the factorial - concise version") (
+    check (
+      obtained = factorial_values
         .map (pair => pair._1 )
-        .map (n => Tuple2 (n, FactorialConcise_ () .get_factorial (n ) ) )
-      assert (obtained == expected ) }
+        .map (n => Tuple2 (n, FactorialConcise_ () .get_factorial (n ) ) ) ) (
+      expected = factorial_values
+    )
+  )
 
-  test ("should test the factorial - verbose version")
-    {
-      lazy val expected = factorial_values
-      lazy val obtained = factorial_values
+  test ("should test the factorial - verbose version") (
+    check (
+      obtained = factorial_values
         .map (pair => pair._1 )
-        .map (n => Tuple2 (n, FactorialVerbose_ () .get_factorial (n ) ) )
-      assert (obtained == expected ) }
+        .map (n => Tuple2 (n, FactorialVerbose_ () .get_factorial (n ) ) ) ) (
+      expected = factorial_values
+    )
+  )
 
-  test ("should test the factorial - with pattern matching")
-    {
-      lazy val expected = factorial_values
-      lazy val obtained = factorial_values
+  test ("should test the factorial - with pattern matching") (
+    check (
+      obtained = factorial_values
         .map (pair => pair._1 )
-        .map (n => Tuple2 (n, FactorialPatternMatching_ () .get_factorial (n ) ) )
-      assert (obtained == expected ) }
+        .map (n => Tuple2 (n, FactorialPatternMatching_ () .get_factorial (n ) ) ) ) (
+      expected = factorial_values
+    )
+  )
 
-  test ("should test the factorial - with fold")
-    {
-      lazy val expected = factorial_values
-      lazy val obtained = factorial_values
+  test ("should test the factorial - with fold") (
+    check (
+      obtained = factorial_values
         .map (pair => pair._1 )
-        .map (n => Tuple2 (n, FactorialWithFold_ () .get_factorial (n ) ) )
-      assert (obtained == expected ) }
+        .map (n => Tuple2 (n, FactorialWithFold_ () .get_factorial (n ) ) ) ) (
+      expected = factorial_values
+    )
+  )
 
 }
--- examples/src/test/scala/soda/example/mathematics/PiIteratorSpec.scala
+++ examples/src/test/scala/soda/example/mathematics/PiIteratorSpec.scala
@@ -5,14 +5,18 @@ case class PiIteratorSpec ()
     org.scalatest.funsuite.AnyFunSuite
 {
 
+  def check [A] (obtained: A ) (expected: A ): org.scalatest.compatible.Assertion =
+    assert (obtained == expected )
+
   lazy val pi_start = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446"
 
   lazy val pi_sequence = PiIterator_ () .take (128 )
 
-  test ("first digits of Pi")
-    {
-      lazy val expected = pi_start
-      lazy val obtained = "" + pi_sequence.head + "." + pi_sequence.tail.mkString ("")
-      assert (obtained == expected ) }
+  test ("first digits of Pi") (
+    check (
+      obtained = "" + pi_sequence.head + "." + pi_sequence.tail.mkString ("") ) (
+      expected = pi_start
+    )
+  )
 
 }
