e72bf245289a3d641683e21731c8c5188c18d4ad
Julian Mendez <julian.mendez@gmail.com>
2022-03-10 11:56:45 +0100

    Remove some unnecessary imports

--- translator/src/main/scala/soda/translator/blocktr/TokenReplacement.scala
+++ translator/src/main/scala/soda/translator/blocktr/TokenReplacement.scala
@@ -3,7 +3,6 @@ package soda.translator.blocktr
 trait TokenReplacement
 {
 
-  import   soda.translator.replacement.ReplacementAux_
   import   soda.translator.replacement.ReplacementWithTranslator_
 
   def replace (table : Seq [ Tuple2 [String, String] ] ) : TokenizedBlockTranslator =
