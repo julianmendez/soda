fa1a1404f1b7242549e9f3322969ba6e1ea30b0f
Julian Mendez <julian.mendez@gmail.com>
2023-03-27 10:47:10 +0200

    Update script to build the project

--- makeall.sh
+++ makeall.sh
@@ -1,18 +1,18 @@
 #/bin/bash
 
 #
-# This script builds the Soda binary file.
+# This script builds the binary file.
 # It requires `sbt` installed.
 #
 
 sbt scalaVersion sbtVersion version clean compile test package assembly
 
-executableStub="exec java -jar \$0 \"\$@\" ; exit"
-sodaFile="soda"
 scalaVersion="3.2.2"
-jarFile="target/scala-${scalaVersion}/soda-*.jar"
+binaryFile="soda"
+executableStub="exec java -jar \$0 \"\$@\" ; exit"
+jarFile="target/scala-${scalaVersion}/${binaryFile}-*.jar"
 
-echo ${executableStub} >${sodaFile}
-cat ${jarFile} >>${sodaFile}
-chmod u+x ${sodaFile}
+echo ${executableStub} >${binaryFile}
+cat ${jarFile} >>${binaryFile}
+chmod u+x ${binaryFile}
 
