7a3514a5549810cda381de40c2aeff4ebdce1c2f
Julian Mendez <julian.mendez@gmail.com>
2024-01-23 18:59:53 +0100

    Update example for Lean

--- examples/src/test/scala/soda/example/forlean/algorithms/Package.scala
+++ examples/src/test/scala/soda/example/forlean/algorithms/Package.scala
@@ -26,16 +26,22 @@ case class MyListSpec ()
 
   lazy val example_list : List [Int] = List (0 , 1 , 1 , 2 , 3 , 5 , 8)
 
+  lazy val empty_int_list : List [Int] = List ()
+
+  lazy val initial_string_list : List [String] = List ("0", "1", "1", "2", "3", "5", "8", "13")
+
+  lazy val empty_string_list : List [String] = List ()
+
   private lazy val _fold_left_initial_value = List [String] ()
 
   private lazy val _fold_left_next_value_function : List [String] => Int => List [String] =
-     (s : List [String]) =>  (e : Int) => s .+: ("" + (e + 100))
+     (s : List [String] ) =>  (e : Int) => s .+: ("" + (e + 100))
 
-  test ("fold left with List") (
+  test ("fold left with a list") (
     check (
       obtained =
         MyList_ ()
-          .foldl (example_list) (_fold_left_initial_value) (
+          .foldl [Int, List [String] ] (example_list) (_fold_left_initial_value) (
             _fold_left_next_value_function)
     ) (
       expected = List ("108" , "105" , "103" , "102" , "101" , "101" , "100")
@@ -47,6 +53,34 @@ case class MyListSpec ()
       obtained = MyList_ () .length (example_list)
     ) (
       expected = toNat (7)
+    )
+  )
+
+  test ("length of an empty list") (
+    check (
+      obtained = MyList_ () .length (empty_int_list)
+    ) (
+      expected = toNat (0)
+    )
+  )
+
+  test ("reverse of a list") (
+    check (
+      obtained =
+        MyList_ ()
+          .reverse [String] (initial_string_list)
+    ) (
+      expected = List [String] ("13" , "8" , "5" , "3" , "2" , "1" , "1", "0")
+    )
+  )
+
+  test ("reverse of an empty list") (
+    check (
+      obtained =
+        MyList_ ()
+          .reverse [String] (empty_string_list)
+    ) (
+      expected = List [String] ()
     )
   )
 
