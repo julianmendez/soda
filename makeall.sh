9702e479c3b1821bd5aed6b217702b7b7b261f09
Julian Mendez <julian.mendez@gmail.com>
2024-03-16 21:38:22 +0100

    Update makeall.sh

--- makeall.sh
+++ makeall.sh
@@ -7,7 +7,7 @@
 
 sbt scalaVersion sbtVersion version clean compile test package assembly
 
-scalaVersion="3.3.1"
+scalaVersion="3.4.0"
 binaryFile="soda"
 executableStub="exec java -jar \$0 \"\$@\" ; exit"
 jarFile="target/scala-${scalaVersion}/${binaryFile}-*.jar"
