c3176b72d335d59bf49ac8292cf9db6c90d5bbc0
Julian Mendez <julian.mendez@gmail.com>
2026-03-11 10:33:14 +0100

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -5,10 +5,10 @@ package «soda» where
   -- add package configuration options here
 
 require batteries from git
-  "https://github.com/leanprover-community/batteries.git" @ "v4.21.0"
+  "https://github.com/leanprover-community/batteries.git" @ "v4.28.0"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.21.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.28.0"
 
 @[default_target]
 lean_lib «Soda» where
