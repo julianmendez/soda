616477225649442f7ff84bcbb1193371bcb93adb
Julian Mendez <julian.mendez@gmail.com>
2026-08-16 08:37:21 +0200

    Use Lean 4.33.0

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «soda» where
   -- add package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.32.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.0"
 
 @[default_target]
 lean_lib «Soda» where
